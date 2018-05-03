955e306a2b42a4f8976e5b0e1576715b3791343c
Kunika92 <kunikasharma92@gmail.com>
2018-05-03 01:08:09 -0700

    new pages
    new pages

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+    $pageTitle = "Contact";
+    $HeroTitle = "Cntact Us";
+    $HeroImg = "defaultBanner.png";
+    include("partials/header.php");
+?>
+        <!-- Main Section -->
+        <main class="container">
+            <!-- Media Gallery -->
+            <section class="row">
+                <article class="col-sm-12 col-md-6 media-section">
+                    <img class="media-img" src="assets/defaultProduct.png" alt ="media 1 image">
+                    <p>Media Title</p>
+                    <p>Media Link</p>
+                </article>
+                <article class="col-sm-12 col-md-6 media-section">
+                    <img class="media-img" src="assets/defaultProduct.png" alt ="media 2 image">
+                    <p>Media Title</p>
+                    <p>Media Link</p>
+                </article>
+            </section>
+        </main>
+    <?php
+
+include("partials/footer.php"); ?>
---
+++ media.php
@@ -0,0 +1,26 @@
+<?php
+    $pageTitle = "Media";
+    $HeroTitle = "Media";
+    $HeroImg = "defaultBanner.png";
+    include("partials/header.php");
+    $arrNavList = $dbControl->getRecord("SELECT * FROM pages_table");
+?>
+        <!-- Main Section -->
+        <main class="container">
+            <!-- Media Gallery -->
+            <section class="row">
+                <article class="col-sm-12 col-md-6 media-section">
+                    <img class="media-img" src="assets/defaultProduct.png" alt ="media 1 image">
+                    <p>Media Title</p>
+                    <p>Media Link</p>
+                </article>
+                <article class="col-sm-12 col-md-6 media-section">
+                    <img class="media-img" src="assets/defaultProduct.png" alt ="media 2 image">
+                    <p>Media Title</p>
+                    <p>Media Link</p>
+                </article>
+            </section>
+        </main>
+    <?php
+
+include("partials/footer.php"); ?>
